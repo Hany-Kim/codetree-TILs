32902dbe6395cb9867747a0f61ca2c5435cbc90b
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2023-12-09 07:35:32 +0900

    Title: 루돌프의 반란, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 231209/루돌프의 반란/rudolph-rebellion.cpp
+++ 231209/루돌프의 반란/rudolph-rebellion.cpp
@@ -6,59 +6,6 @@
 using namespace std;
 #define DUMMY 213456789
 
-
-/*
-상황
-1번부터 P번까지 P 명의 산타들이 크리스마스 이브를 준비하던 중, 
-산타의 주요 수송수단인 루돌프가 반란을 일으켰습니다. 
-루돌프는 산타들을 박치기하여 산타의 배달을 방해
-산타들은 루돌프를 잡아는다.
-*/
-
-// 게임 구성
-// N*N 격자 (r,c) 좌상단(1,1)
-// M개의 턴동안 루돌프와 이동 가능한 모든 산타 1번씩 이동
-// 두칸 사이의 거리 (r1 - r2)^2 + (c1 - c2)^2
-
-// 루돌프 움직임
-// 루돌프는 가장 가까운 탈락하지 않은 산타로 1칸 돌진
-// if 가까운 산타 2명 r좌표가 큰 산타, r좌표 동일하다면 c좌표 큰 산타
-// 루돌프는 상하좌우, 대각성 8방향이동가능
-
-// 산타의 움직임
-// 1~P번까지 순서대로 움직임
-// 기절했거나 이미 게임에서 탈락했다면 움직일 수 없음
-// 산타는 루돌프에게 거리가 가장 가까워지는 방향으로 1칸 이동
-// 산타는 다른 산타가 있는 칸이나 게임판 밖으로 움직일 수 없다.
-// 움직일 수 있는 칸이 없다면 산타는 움직이지 않는다.
-// 움직일 수 있는 칸이 있더라도 만약 루돌프로부터 가까워질 수 있는 방법이 없다면 산타는 움직이지 않는다.
-// 산타는 상하좌우 4방향 이동 가능, 이동 가능 방향 여러개라면 상하좌우 우선순위에 맞춰 움직
-
-// 충돌
-// """산타와 루돌프가 같은 칸"""
-// 루돌프가 움직여서 충돌 발생 경우, 해당 산타는 C만큼의 점수를 얻는다. 동시에 산타는 루돌프가 이동해온 방향으로 C칸만큼 밀린다.
-// 산타가 움직여서 충돌 발생 경우, 해당 산타는 D만큼의 점수를 얻는다. 동시에 자신이 이동해온 반대방향으로 D칸만큼 밀린다.
-// 밀려나는 것은 포물선 모양을 그리며 밀리기 때문에, "도중에 충돌이 일어나지 않는다."
-// 밀려난 위치가 게임판 밖이라면 산타는 탈락
-// 밀려난 위치에 다른 산타가 있다면 상호작용 발생
-
-// 상호작용
-// """루돌프와 충돌후 착지한 칸에서만""" 상호작용 발생
-// 다른 산타 있다면, 그 산타는 1칸 해당 방향으로 밀림
-// 그 옆에 산타가 있다면 연쇄적으로 1칸씩 밀림
-// 게임판 밖으로 밀리면 탈락
-
-// 기절
-// 산타는 루돌프와 충돌 후 기절
-// 현재 k턴이라면 (k+1)턴까지 기절, (k+2)턴부터 다시 정상
-// 기절한 산타는 움직일 수 없음
-// 루돌프는 기절한 산타를 돌진대상으로 선택할 수 있음
-
-// 게임종료
-// M턴이 지남
-// P명의 산타 모두 탈락
-// 매턴 이후 아직 탈락하지 않은 산타들에게 1점씩 부여
-
 int N, M, P, C, D;
 struct POINT {
     int y;
@@ -75,8 +22,8 @@ struct INFO {
 };
 INFO santa[31];
 int map[51][51];
-int dy[8] = { -1,1,0,0,-1,-1,1,1 };
-int dx[8] = { 0,0,-1,1,-1,1,-1,1 };
+int dy[8] = { -1,0,1,0,-1,-1,1,1 };
+int dx[8] = { 0,1,0,-1,-1,1,-1,1 };
 int cur_turn;
 
 void omap() {
@@ -239,7 +186,7 @@ void move_rudolph() {
 
 void santa_to_rudolph(int num) {
     POINT next = { santa[num].y, santa[num].x }; // 산타가 이동할 위치
-    int min_dist = DUMMY;
+    int min_dist = get_dist(santa[num].y, santa[num].x, rudolph.y, rudolph.x);;
     int direct_num = 0;
 
     for (int i = 0; i < 4; ++i) {
@@ -251,20 +198,22 @@ void santa_to_rudolph(int num) {
         int cur_dist = get_dist(ny, nx, rudolph.y, rudolph.x);
         if (min_dist > cur_dist) {
             min_dist = cur_dist;
+
             next.y = ny;
             next.x = nx;
             direct_num = i;
         }
     }
 
+
     if ((rudolph.y == next.y) && (rudolph.x == next.x)) { // 산 -> 루 충돌
         // 충돌
         santa[num].score += D;
             
-        if (direct_num == 0) direct_num = 1;
-        else if (direct_num == 1) direct_num = 0;
-        else if (direct_num == 2) direct_num = 3;
-        else if (direct_num == 3) direct_num = 2;
+        if (direct_num == 0) direct_num = 2;
+        else if (direct_num == 2) direct_num = 0;
+        else if (direct_num == 1) direct_num = 3;
+        else if (direct_num == 3) direct_num = 1;
 
         int ny = next.y + (dy[direct_num] * D);
         int nx = next.x + (dx[direct_num] * D);
@@ -347,6 +296,10 @@ int main() {
         //cout << "현재 턴 : " << i << "\n";
         move();
         add_score();
+        //cout << "생존 산타 +1\n";
+        //output();
+        //cout << "\n";
+        //cout << "\n";
     }
 
     output();
