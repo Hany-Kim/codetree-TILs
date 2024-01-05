2020dd7ee6a4d99b086d11d37218ccb1023f9001
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-01-05 23:07:30 +0900

    Title: 문자열을 출력하는 재귀함수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/문자열을 출력하는 재귀함수/recursive-function-that-outputs-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << "Coding is my favorite hobby!\n";
+    }
+    return 0;
+}
