8a1989ade77cfccd0efa354341789ec0902ea039
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-02-22 21:41:49 +0900

    Title: 코드트리 오마카세, Time: 722ms, Memory: 45MB, Status: Passed - CodeTree

--- 240222/코드트리 오마카세/codetree-omakase.cpp
+++ 240222/코드트리 오마카세/codetree-omakase.cpp
@@ -15,16 +15,14 @@ struct CMD {
     string name;
 };
 vector<CMD> commands;
-//vector<CMD> create_sushi;
-unordered_map<string, vector<CMD>> create_sushi;
-unordered_map<string, int> sitting_position; // 사람이 앉은 자리 매핑
-unordered_set<string> visitting_customer_names; // 방문한 사람 저장 ( 재방문은 없으므로 중복이 없다 )
-unordered_map<string, int> visit_customer_time;
-unordered_map<string, int> leave_customer_time;
-unordered_map<string, int> eat_cnt;
+unordered_map<string, vector<CMD>> create_sushi; // 시간초과 관리 (초밥생성 명령을 따로 분리)
+unordered_map<string, int> sitting_position; // 손님이 앉은 자리 매핑
+unordered_set<string> visitting_customer_names; // 방문한 손님 목록 저장 ( 재방문은 없으므로 중복이 없다 )
+unordered_map<string, int> visit_customer_time; // 손님이 방문한 시간대
+unordered_map<string, int> eat_cnt; // 손님이 먹어야할 초밥 갯수 매핑
 vector<CMD> v;
-int sushi_cnt;
-int customer_cnt;
+int sushi_cnt; // 남은 초밥 갯수
+int customer_cnt; // 남은 손님 명수
 
 void input() {
     cin >> L >> Q;
@@ -36,13 +34,18 @@ void input() {
 
         if (cmd == 100) {
             cin >> t >> x >> name;
+            /*
+            시간 초과 관리를 위해 초밥생성 관련 명령 분리
+
+            최대 명령 수 : 100,000
+            회대 사람 수 : 15,000
+            */
             CMD tmp1;
             tmp1.cmd = cmd;
             tmp1.t = t;
             tmp1.x = x;
             tmp1.name = name;
             tmp1.n = n;
-            //create_sushi.push_back(tmp1);
             create_sushi[name].push_back(tmp1);
         }
         else if (cmd == 200) {
@@ -87,40 +90,86 @@ bool mcmp(CMD left, CMD right) {
 
 void sol() {
     for (string now_name : visitting_customer_names) {
-        leave_customer_time[now_name] = -1;
-
-        //FOR(i, 0, create_sushi.size()) { // 생성된 초밥이 언제 없어질지 기록
         for(CMD sushis : create_sushi[now_name]) {// 생성된 초밥이 언제 없어질지 기록
-            //if (mstrcmp(create_sushi[i].name, now_name) == false) continue;
+            // 해당 손님(now_name)이 먹어야할 초밥
+            /*
+                시간 초과 관리를 위해 초밥생성 관련 명령 분리
+
+                최대 명령 수 : 100,000
+                회대 사람 수 : 15,000
+            */
 
             int meet_sushi_cutomer_time = 0; // 초밥과 손님이 만나게 할 수 있는 보정 시간
             int eat_sushi_time = 0; //초밥을 먹는 시간
-            if (sushis.t < visit_customer_time[now_name]) { // 손님 입장전에 손님의 초밥이 벨트위에 있음
+
+            if (sushis.t < visit_customer_time[now_name]) { 
+                /*
+                    손님 입장전에 손님의 초밥이 벨트위에 있음
+                */
+
+                // 현재 초밥의 위치
                 int now_sushi_position = sushis.x;
 
+                // 손님이 방문한 시점과 먹어야할 초밥이 들어온 시점의 차이
                 int diff_t = visit_customer_time[now_name] - sushis.t;
-                now_sushi_position = ((now_sushi_position + diff_t) % L); // 손님이 입장했을때, 회전 초밥 위치 이동
+
+
+                // 손님이 입장했을때, 회전 초밥 위치 이동
+                // 
+                // 손님이 입장하기 전에 먹어야할 초밥이 들어와있으므로
+                // 손님이 들어올 시점에 초밥이 어디로 이동할지 구해줌
+                now_sushi_position = ((now_sushi_position + diff_t) % L);
 
                 if (sitting_position[now_name] >= now_sushi_position) {
+                    /*
+                        손님이 초밥보다 시계방향으로 더 먼쪽에 앉아 있음
+                    */
+
+                    // 손님이 초밥을 먹기위해 걸리는 시간 = 손님이 앉은 자리 위치 - 현재 초밥 위치
                     meet_sushi_cutomer_time = sitting_position[now_name] - now_sushi_position;
                 }
                 else if (sitting_position[now_name] < now_sushi_position) {
+                    /*
+                        손님이 초밥보다 반시계방향으로 더 먼쪽에 앉아 있음
+                    */
+
+                    // 손님이 초밥을 먹기위해 걸리는 시간 = (손님이 앉은 자리 위치 + 의자의 갯수(1싸이클)) - 현재 초밥 위치
                     meet_sushi_cutomer_time = (sitting_position[now_name] + L) - now_sushi_position;
                 }
+
+                // 손님을 초밥을 먹을 수 있는 시간 = 손님이 방문한 시간대 + 손님이 초밥을 먹기위해 걸리는 시간
                 eat_sushi_time = visit_customer_time[now_name] + meet_sushi_cutomer_time;
             }
-            else { // 손님 입장 후 초밥이 벨트위로 올라옴
+            else { 
+                /*
+                    손님 입장 후 초밥이 벨트위로 올라옴
+                */
+
+                // 현재 초밥의 위치
                 int now_sushi_position = sushis.x;
 
                 if (sitting_position[now_name] >= now_sushi_position) {
+                    /*
+                        손님이 초밥보다 시계방향으로 더 먼쪽에 앉아 있음
+                    */
+
+                    // 손님이 초밥을 먹기위해 걸리는 시간 = 손님이 앉은 자리 위치 - 현재 초밥 위치
                     meet_sushi_cutomer_time = sitting_position[now_name] - now_sushi_position;
                 }
                 else if (sitting_position[now_name] < now_sushi_position) {
+                    /*
+                        손님이 초밥보다 반시계방향으로 더 먼쪽에 앉아 있음
+                    */
+
+                    // 손님이 초밥을 먹기위해 걸리는 시간 = (손님이 앉은 자리 위치 + 의자의 갯수(1싸이클)) - 현재 초밥 위치
                     meet_sushi_cutomer_time = (sitting_position[now_name] + L) - now_sushi_position;
                 }
+
+                // 손님을 초밥을 먹을 수 있는 시간 = 초밥이 들어온 시간 + 손님이 초밥을 먹기위해 걸리는 시간
                 eat_sushi_time = sushis.t + meet_sushi_cutomer_time;
             }
 
+            // 초밥이 사라지는 시점을 저장
             CMD tmp;
             tmp.cmd = sushis.cmd + 1;
             tmp.t = eat_sushi_time;
@@ -131,24 +180,46 @@ void sol() {
         }
     }
 
+    // 전체 명령과 초밥이 사라지는 시점에 대한 정보를 합침
     commands.insert(commands.end(), v.begin(), v.end());
+
+    // t가 작을 수록, 명령이 작을수록 우선순위를 부여해 정렬함
+    // 명령 우선순위 (100 -> 101 -> 200 -> 300)
     sort(commands.begin(), commands.end(), mcmp);
 
     FOR(i, 0, commands.size()) {
+        /*
+            전체 명령과 초밥이 사라지는 시점에 대한 정보를 합친것을 바탕으로
+            cmd에 따른 작업을 수행
+        */
+
         if (commands[i].cmd == 100) {
+            /*
+                초밥이 생성됨
+            */
             ++sushi_cnt;
         }
         else if (commands[i].cmd == 101) {
+            /*
+                초밥이 사라짐
+            */
             --sushi_cnt;
 
+            // 초밥이 사라졌으므로 손님을 먹을 초밥 갯수를 줄임
             string now_name = commands[i].name;
             eat_cnt[now_name] = eat_cnt[now_name] - 1;
-            if (eat_cnt[now_name] <= 0) --customer_cnt;
+            if (eat_cnt[now_name] <= 0) --customer_cnt; // 손님이 먹을 초밥의 갯수가 0이하가 되었다면 방문한 손님 수는 줄어듬
         }
         else if (commands[i].cmd == 200) {
+            /*
+                손님이 방문함
+            */
             ++customer_cnt;
         }
         else if (commands[i].cmd == 300) {
+            /*
+                촬영
+            */
             cout << customer_cnt << " " << sushi_cnt << '\n';
         }
     }
@@ -156,7 +227,6 @@ void sol() {
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    //freopen("input.txt", "r", stdin);
     input();
     sol();
 
