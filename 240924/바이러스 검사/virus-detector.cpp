79c68198aaa455c1f1d6e61c769a1c65a9b0de0e
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-09-24 22:13:26 +0900

    Title: 바이러스 검사, Time: 488ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 240924/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define N_MAX 1000000
+
+int n;
+int arr[N_MAX];
+int teamLeader, teamMember;
+int ans;
+
+void hasTeamLeader() {
+	for (int i = 0; i < n; ++i) {
+		if (arr[i] > 0) {
+			arr[i] -= teamLeader;
+			ans += 1;
+		}
+	}
+}
+
+void hasTeamMember() {
+	for (int i = 0; i < n; ++i) {
+		if (arr[i] > 0) {
+			ans += (arr[i] / teamMember);
+			if (arr[i] % teamMember > 0) ans += 1;
+		}
+	}
+}
+
+void in() {
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+	cin >> teamLeader >> teamMember;
+}
+void sol() {
+	hasTeamLeader();
+	hasTeamMember();
+
+	cout << ans;
+}
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+
+	in();
+	sol();
+
+	return 0;
+}
