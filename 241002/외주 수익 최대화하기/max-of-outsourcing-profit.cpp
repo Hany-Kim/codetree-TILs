93c27e871c29a35535976cdee8ee178a51dd2b43
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-10-02 11:18:21 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241002/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define N_MAX (15 + 1)
+
+struct TASK {
+	int day, reward;
+};
+int N;
+int date[N_MAX];
+TASK arr[N_MAX];
+int maxReward;
+
+void dfs(int to, int rew) {
+	maxReward = max(rew, maxReward);
+
+	for (int i = to + 1; i <= N; ++i) {
+		if (date[i] == 1) continue;
+		TASK now = arr[i];
+
+		for (int j = 0; j < now.day; ++j) {
+			date[i + j] = 1;
+		}
+
+		dfs(i, rew + now.reward);
+		
+		for (int j = 0; j < now.day; ++j) {
+			date[i + j] = 0;
+		}
+	}
+}
+
+void sol() {
+	dfs(0, 0);
+	cout << maxReward;
+}
+
+void in() {
+	cin >> N;
+	for (int i = 1; i <= N; ++i) {
+		cin >> arr[i].day >> arr[i].reward;
+	}
+}
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+	in();
+	sol();
+	return 0;
+}
