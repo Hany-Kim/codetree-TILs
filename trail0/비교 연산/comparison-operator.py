58b6b27205db72b7e8e841d65f615d0a431b48c4
Hans <hanykim1210@gmail.com>
2026-06-05 16:50:33 +0900

    [Passed] 비교 연산 (47ms, 16MB)

---
+++ trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+
+print(1 if (a >= b) else 0)
+print(1 if (a > b) else 0)
+print(1 if (a <= b) else 0)
+print(1 if (a < b) else 0)
+print(1 if (a == b) else 0)
+print(1 if (a != b) else 0)
