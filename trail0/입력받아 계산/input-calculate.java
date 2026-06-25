8b21adf7d622f2e250e602583c14840efb88f6e6
Hans <hanykim1210@gmail.com>
2026-06-25 09:16:13 +0900

    [Passed] 입력받아 계산 (132ms, 10MB)

---
+++ trail0/입력받아 계산/input-calculate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.print(n + 2);
+    }
+}
