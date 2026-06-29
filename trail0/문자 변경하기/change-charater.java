94f9c2fa816f1a253761dd35890b1d6868cc71c0
Hans <hanykim1210@gmail.com>
2026-06-29 10:13:29 +0900

    [Passed] 문자 변경하기 (88ms, 9MB)

---
+++ trail0/문자 변경하기/change-charater.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char a = 'C';
+        a = 'T';
+
+        System.out.println(a);
+    }
+}
