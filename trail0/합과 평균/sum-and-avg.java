3aedefc2c9498f1b85137c2ac0d2b39faf12deb2
Hans <hanykim1210@gmail.com>
2026-07-08 15:50:47 +0900

    [Passed] 합과 평균 (149ms, 10MB)

---
+++ trail0/합과 평균/sum-and-avg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        int sum = a + b;
+        double avg = sum / 2.0;
+        System.out.print(sum + " " + avg);
+    }
+}
