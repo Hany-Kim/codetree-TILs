0c44fa200805240c62a6911506f01c17036bae36
Hans <hanykim1210@gmail.com>
2026-07-08 15:28:41 +0900

    [Passed] 달에서 무게 구하기 (100ms, 9MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int w = 13;
+        double g = 0.165;
+
+        double res = w * g;
+
+        System.out.printf(w + " * %.6f = %.6f", g, res);
+    }
+}
