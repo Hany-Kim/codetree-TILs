9733bdddc3332017e5bdab49473c0d59ae2c568c
Hans <hanykim1210@gmail.com>
2026-07-08 15:48:35 +0900

    [Passed] 입력받은 값과 합 출력 (138ms, 10MB)

---
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a + " " + b + " " + (a+b));
+    }
+}
