0e5f2b2af8ef1a2a6f9632140efcc769d3487044
Hans <hanykim1210@gmail.com>
2026-07-08 15:36:27 +0900

    [Passed] 점수 입출력 (142ms, 10MB)

---
+++ trail0/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        System.out.println("Your score is " + s + " point.");
+    }
+}
