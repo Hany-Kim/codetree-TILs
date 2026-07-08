760a053ce334744bca6f3cd5bc2febb3abc7a94a
Hans <hanykim1210@gmail.com>
2026-07-08 15:22:51 +0900

    [Passed] 변수 출력하기 3 (101ms, 9MB)

---
+++ trail0/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+
+        System.out.print(a + "->" + b + "->" + c);
+    }
+}
