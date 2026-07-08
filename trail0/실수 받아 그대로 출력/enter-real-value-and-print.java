bc23a2ae29f7b70db515bb64375a17661f3c72c7
Hans <hanykim1210@gmail.com>
2026-07-08 15:38:58 +0900

    [Passed] 실수 받아 그대로 출력 (164ms, 12MB)

---
+++ trail0/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+
+        System.out.printf("%.2f", d);
+    }
+}
